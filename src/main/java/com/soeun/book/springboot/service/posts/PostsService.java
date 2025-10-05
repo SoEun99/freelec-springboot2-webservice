a6e529dc88d5c02d5087aab9acc84ece731e7bea
SuHyunYaaang <eoth999@gmail.com>
2025-10-05 11:22:51 +0900

    Function Creation

---
+++ src/main/java/com/soeun/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,21 @@
+package com.soeun.book.springboot.service.posts;
+
+import com.soeun.book.springboot.domain.posts.PostsRepository;
+import com.soeun.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+
+    }
+
+}
