4dd7c6009731b5ade1582677abb4d033217df48b
SuHyunYaaang <eoth999@gmail.com>
2025-10-07 14:07:23 +0900

    JPA Practice Finished

--- src/main/java/com/soeun/book/springboot/Application.java
+++ src/main/java/com/soeun/book/springboot/Application.java
@@ -4,9 +4,11 @@ package com.soeun.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 // ┖ 스프링 부트 자동 설정, 스프링 Bean 읽기와 생성을 모두 자동으로 설정
 // @SpringBootApplication 위치부터 설정 읽음, 때문에 항상 최상단에 위치
 
+@EnableJpaAuditing //JPA Auditing 활성화
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
