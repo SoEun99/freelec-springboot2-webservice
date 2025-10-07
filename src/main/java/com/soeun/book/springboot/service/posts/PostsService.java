47cfb79012c82aebb2fe2ff6dcb57b61f2846fad
SuHyunYaaang <eoth999@gmail.com>
2025-10-07 18:20:49 +0900

    Making post modifying Page

--- src/main/java/com/soeun/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/soeun/book/springboot/service/posts/PostsService.java
@@ -18,24 +18,23 @@ public class PostsService {
 
     private final PostsRepository postsRepository;
 
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+
+    }
 
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
 
         Posts posts = postsRepository.findById(id)
-                .orElseThrow(() -> new
-                        IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
 
     }
 
-    @Transactional
-    public Long save(PostsSaveRequestDto requestDto) {
-        return postsRepository.save(requestDto.toEntity()).getId();
-
-    }
 
     public PostsResponseDto findById (Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new
